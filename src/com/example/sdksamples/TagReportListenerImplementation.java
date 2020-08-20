a051a9b6e83a1631f5b639caa9efa7e8f3653c89
niklas <niklasp.hastbacka@gmail.com>
2020-08-20 12:53:44 +0300

    first commit

---
+++ src/com/example/sdksamples/TagReportListenerImplementation.java
@@ -0,0 +1,113 @@
+package com.example.sdksamples;
+
+import com.impinj.octane.ImpinjReader;
+
+import com.impinj.octane.Tag;
+import com.impinj.octane.TagReport;
+import com.impinj.octane.TagReportListener;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Date;
+import java.util.Calendar;
+
+import java.util.List;
+
+public class TagReportListenerImplementation implements TagReportListener {
+
+    @Override
+    public void onTagReported(ImpinjReader reader, TagReport report) {
+        List<Tag> tags = report.getTags();
+
+        for (Tag t : tags) {
+        	String epc = t.getEpc().toString();
+            System.out.print(" EPC: " + epc);
+            
+            try {
+                String rfid = "RFID1";
+                Date date = Calendar.getInstance().getTime();
+                long timeMilli = date.getTime();
+
+                URL url = new URL("http://localhost/rfid/" + epc + "/" + timeMilli);
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setRequestMethod("GET");
+                conn.setRequestProperty("Accept", "application/json");
+
+                if (conn.getResponseCode() != 200) {
+                    throw new RuntimeException("Failed : HTTP error code : "
+                            + conn.getResponseCode());
+                }
+
+                BufferedReader br = new BufferedReader(new InputStreamReader(
+                    (conn.getInputStream())));
+                String output;
+                System.out.println("Output from Server .... \n");
+                while ((output = br.readLine()) != null) {
+                    System.out.println(output);
+                }
+                conn.disconnect();
+              } catch (MalformedURLException e) {
+                e.printStackTrace();
+              } catch (IOException e) {
+                e.printStackTrace();
+              }
+
+            if (reader.getName() != null) {
+                System.out.print(" Reader_name: " + reader.getName());
+            } else {
+                System.out.print(" Reader_ip: " + reader.getAddress());
+            }
+
+            if (t.isAntennaPortNumberPresent()) {
+                System.out.print(" antenna: " + t.getAntennaPortNumber());
+            }
+
+            if (t.isFirstSeenTimePresent()) {
+                System.out.print(" first: " + t.getFirstSeenTime().ToString());
+            }
+
+            if (t.isLastSeenTimePresent()) {
+                System.out.print(" last: " + t.getLastSeenTime().ToString());
+            }
+
+            if (t.isSeenCountPresent()) {
+                System.out.print(" count: " + t.getTagSeenCount());
+            }
+
+            if (t.isRfDopplerFrequencyPresent()) {
+                System.out.print(" doppler: " + t.getRfDopplerFrequency());
+            }
+
+            if (t.isPeakRssiInDbmPresent()) {
+                System.out.print(" peak_rssi: " + t.getPeakRssiInDbm());
+            }
+
+            if (t.isChannelInMhzPresent()) {
+                System.out.print(" chan_MHz: " + t.getChannelInMhz());
+            }
+
+            if (t.isRfPhaseAnglePresent()) {
+                System.out.print(" phase angle: " + t.getPhaseAngleInRadians());
+            }
+
+            if (t.isFastIdPresent()) {
+                System.out.print("\n     fast_id: " + t.getTid().toHexString());
+
+                System.out.print(" model: " +
+                        t.getModelDetails().getModelName());
+
+                System.out.print(" epcsize: " +
+                        t.getModelDetails().getEpcSizeBits());
+
+                System.out.print(" usermemsize: " +
+                        t.getModelDetails().getUserMemorySizeBits());
+            }
+
+            System.out.println("");
+        }
+        
+    }
+}
